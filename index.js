d5f6d7a7d70c7253e9fb178eccec0c0b62ae06f2
Michael-AYK <71391713+Michael-AYK@users.noreply.github.com>
2021-01-03 15:20:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ expressServeur.listen(process.env.PORT || 3000, () => {
                 IdEvenement: IdEvenement, IdEtab: IdEtab, type: type
               })
             })
-            .then(d => d.json())
+            .then(d => d.text())
             .catch(function (error) {
               console.log(error);
             })
