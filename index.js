894da821fd7e1b58cb9876a3851f8d57803c3a08
Michael-AYK <71391713+Michael-AYK@users.noreply.github.com>
2021-10-29 09:35:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ expressServeur.listen(process.env.PORT || 3000, () => {
           const IdEvenement = gettingIdEvent[1]
           console.log('IDEVENT ICI')
           let tokens = [];
-          fetch('https://www.ogseic.com/SuiviApprenants/myBackEnd_m_pes/getTokens.php', {
+          fetch('https://www.ogseic.com/SuiviApprenants/myBackEnd_m_pes/test/getTokens.php', {
               method: 'post',
               headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
               body:JSON.stringify({
